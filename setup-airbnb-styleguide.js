e0989b110f87f7085f9710485bc60453a6459a79
Pjaerr <washingtonfaulconer@gmail.com>
2019-03-02 15:27:54 +0000

    Made script actually use itself

--- setup-airbnb-styleguide.js
+++ setup-airbnb-styleguide.js
@@ -1,6 +1,6 @@
-const { exec } = require("child_process");
-const fs = require("fs");
-const ora = require("ora");
+const { exec } = require('child_process');
+const fs = require('fs');
+const ora = require('ora');
 
 const eslintrc = `
 {
@@ -19,80 +19,65 @@ const prettierrc = `
 `;
 
 console.log(
-  "Ensure you have installed the Prettier extension for VSCode:\nhttps://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode \n\n"
+  'Ensure you have installed the Prettier extension for VSCode:\nhttps://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode \n\n'
 );
 
-const spinner = ora("npm install -D eslint prettier\n").start();
+const spinner = ora('npm install -D eslint prettier\n').start();
 
-exec("npm install -D eslint prettier", (err, stdout, stderr) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
+exec('npm install -D eslint prettier', (err, stdout, stderr) => {
+  logExecInformation(err, stdout, stderr);
 
-  console.log(stdout);
-  console.log(stderr);
+  spinner.text = 'npx install-peerdeps --dev eslint-config-airbnb\n';
 
-  spinner.text = "npx install-peerdeps --dev eslint-config-airbnb\n";
+  exec('npx install-peerdeps --dev eslint-config-airbnb', (err, stdout, stderr) => {
+    logExecInformation(err, stdout, stderr);
 
-  exec(
-    "npx install-peerdeps --dev eslint-config-airbnb",
-    (err, stdout, stderr) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
+    spinner.text = 'npm install -D eslint-config-prettier eslint-plugin-prettier\n';
 
-      console.log(stdout);
-      console.log(stderr);
+    exec('npm install -D eslint-config-prettier eslint-plugin-prettier', (err, stdout, stderr) => {
+      logExecInformation(err, stdout, stderr);
 
-      spinner.text =
-        "npm install -D eslint-config-prettier eslint-plugin-prettier\n";
-
-      exec(
-        "npm install -D eslint-config-prettier eslint-plugin-prettier",
-        (err, stdout, stderr) => {
-          if (err) {
-            console.log(err);
-            return;
-          }
-
-          console.log(`stdout: ${stdout}`);
-          console.log(`stderr: ${stderr}`);
-
-          createESLintRc()
-            .then(createPrettierRc)
-            .then(() => {
-              spinner.stop();
-              console.log(
-                "\n\nProject setup to use the Airbnb Style Guide with Prettier and ESLint!\n\n"
-              );
-            })
-            .catch(err => console.error(err));
-        }
-      );
-    }
-  );
+      createESLintRc()
+        .then(createPrettierRc)
+        .then(() => {
+          spinner.stop();
+          console.log(
+            '\n\nProject setup to use the Airbnb Style Guide with Prettier and ESLint!\n\n'
+          );
+        })
+        .catch(err => console.error(err));
+    });
+  });
 });
 
 const createESLintRc = () => {
-  spinner.text = "Creating .eslintrc.json";
+  spinner.text = 'Creating .eslintrc.json';
   return new Promise((resolve, reject) => {
-    fs.writeFile(".eslintrc.json", eslintrc, function(err, data) {
+    fs.writeFile('.eslintrc.json', eslintrc, function(err, data) {
       if (err) reject(err);
-      console.log(".eslintrc.json created");
+      console.log('.eslintrc.json created');
       resolve();
     });
   });
 };
 
 const createPrettierRc = () => {
-  spinner.text = "Creating .prettierrc";
+  spinner.text = 'Creating .prettierrc';
   return new Promise((resolve, reject) => {
-    fs.writeFile(".prettierrc", prettierrc, function(err, data) {
+    fs.writeFile('.prettierrc', prettierrc, function(err, data) {
       if (err) reject(err);
-      console.log(".prettierrc created");
+      console.log('.prettierrc created');
       resolve();
     });
   });
+};
+
+const logExecInformation = (err, stdout, stderr) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
 };
